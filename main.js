89c9fad7c0f4b2bbeffbc1e78c73878ca1431e36
thoma138 <thomaselliott@live.com>
2022-04-01 15:32:37 +1100

    3rd commit

--- main.js
+++ main.js
@@ -10,28 +10,18 @@ function displayImg(imgUrl, elt) {
 
     imgContainer.classList.add('displayMe');
 
-    imgContainer.style.top = (elt.offset + 40) + 'px';
+    imgContainer.style.top = (elt.offset + 5) + 'px';
 
     imgContainer.style.left = elt.offsetLeft + 'px';
     
     let difference = document.documentElement.clientWidth - elt.offsetLeft;
 
-    if(difference < 250) {
 
-        imgContainer.style.right = '20px';
-        imgContainer.style.left = 'auto';
-        
-    } else {
-
-        imgContainer.style.left = elt.offsetLeft + 'px';
-    }
-    
-    imgContainer.style.animation = 'fadeIn .8s';
 }
 
 function removeImg() {
 
-    imgContainer.style.animation = 'fadeOut .8s';
+    imgContainer.style.animation = 'fadeOut 8s';
 
     setTimeout(() => {
         body.removeChild(imgContainer);
